337d883d5f13d0fec20f089f6c1cbb7be8cfe15d
Apfeitoza <annapaulaasf@gmail.com>
2026-04-02 17:27:50 -0300

    adicionados slides de fotos

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import ScrollAnima from "./modules/scroll-anima.js";
-
+import SlideNav from "./modules/slide.js";
 
 const scrollSuave = new ScrollSuave("[data-menu='suave'] a[href^='#']");
 scrollSuave.init();
@@ -42,10 +42,12 @@ scrollAnima.init();
 const dropdownMenu = new DropdownMenu("[data-dropdown]");
 dropdownMenu.init();
 
-const menuMobile = new MenuMobile('[data-menu="button"]','[data-menu="list"]');
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
 const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
 
-
+const slide = new SlideNav(".slide", ".wrapper");
+slide.init();
+slide.addControl(".custom-controls");
